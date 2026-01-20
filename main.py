5e8476c9dfd1f4ce5bb42c40392ee0d59e8ed13f
IliasSari <sarigilias@gmail.com>
2026-01-20 18:25:05 +1030

    Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
+    formated_f_name = (f_name.title())
+    formated_l_name = (l_name.title())
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is your first name?"), input("What is your last name?")))
+
+output = len("Ilias")
+
+
+#Other lesson!
+
+
+def function_1(text):
+    return text + text
+
+def function_2(text):
+    return text.title()
+
+output = function_2(function_1("hello"))
+print(output)
